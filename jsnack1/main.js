9a7aa5b9d34334bd4d8258ab764a187f05dbc6b0
thrudr <ariel.oliveiraesilva@gmail.com>
2023-04-03 15:18:37 +0200

    parseInt-numbers

--- jsnack1/main.js
+++ jsnack1/main.js
@@ -2,3 +2,7 @@
 let firstNumber = prompt("Choose a number:");
 let secondNumber = prompt("Choose another number:");
 
+let number1 = parseInt(firstNumber);
+let number2 = parseInt(secondNumber);
+
+// CONDITION
