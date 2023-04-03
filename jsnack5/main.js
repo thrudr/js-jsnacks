521d6d2d12c1a8ecf6b29115f5dc3b9cc6319b2f
thrudr <ariel.oliveiraesilva@gmail.com>
2023-04-03 17:27:58 +0200

    exercise-completed

--- jsnack5/main.js
+++ jsnack5/main.js
@@ -4,4 +4,23 @@ Chiedi per 6 volte all’utente di inserire un numero,
 se è dispari inseriscilo nell’array. Stampa l'array alla fine.
 */
 
-//
+// ARRAY
+let oddArray = [];
+let evenArray = [];
+
+// USER NUMBER
+for (let i = 0; i < 6; i++){
+    let number =prompt("Choose a number:");
+    number = parseInt(number);
+    
+    if (number % 2 != 0){
+        oddArray.push(number);
+
+    } else {
+        evenArray.push(number);
+
+    }
+
+}
+console.log("Your numbers is: " + oddArray + "," + evenArray);
+console.log("Your odd numbers is: " + oddArray);
