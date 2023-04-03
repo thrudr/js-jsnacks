7c377cb1e66d68ccff42db8ac1793d01175b4f02
thrudr <ariel.oliveiraesilva@gmail.com>
2023-04-03 18:01:48 +0200

    multiplication-table

---
+++ jsnack7/main.js
@@ -0,0 +1,12 @@
+/*
+Scrivere un programma che stampi la tabellina del 2, fino al numero 1000.
+Modificate poi il programma in modo che venga chiesto all'utente il
+numero massimo consentito, e stampare tutta la tabellina del 2 fino al
+numero inserito.
+*/
+
+
+// MULTIPLICATION TABLE
+for (let i=2; i<=1000; i=i+2){
+    console.log(i);
+}
