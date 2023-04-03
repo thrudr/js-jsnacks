4c20fd5512a70fd42f08072506c2853eaee48144
thrudr <ariel.oliveiraesilva@gmail.com>
2023-04-03 15:36:07 +0200

    consition-if

--- jsnack1/main.js
+++ jsnack1/main.js
@@ -6,3 +6,16 @@ let number1 = parseInt(firstNumber);
 let number2 = parseInt(secondNumber);
 
 // CONDITION
+if (number1 > number2){
+    console.log(number1);
+    console.log("The first number is larger than the second number.");
+
+} else if (number2 > number1){
+    console.log(number2);
+    console.log("The second number is larger than the second number.");
+
+} else {
+    console.log(number1, number2);
+    console.log("The numbers are equal.");
+}
+
